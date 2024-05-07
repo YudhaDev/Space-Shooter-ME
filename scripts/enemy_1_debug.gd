1daac79f4e1545398a545f39c5c6550069689aa7
YudhaDev <33086490+YudhaDev@users.noreply.github.com>
2024-05-07 22:50:55 +0700

    simple navmesh enemy movement to player

--- scripts/enemy_1_debug.gd
+++ scripts/enemy_1_debug.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var base_speed:float = 1
 var max_speed:float = 200
+var static_speed = 50.0
+
 var rng_speed:int = 0
 var base_health = 40
 var health_modifier = 1
@@ -12,7 +14,7 @@ var rng_direction_x:float = 0
 @export var object_to_go:Node2D
 @export var navigation_agent :NavigationAgent2D
 var navigation_agent_path_desired_distance = 4.0
-var navigation_agent_target_desired_distance = 4.0
+var navigation_agent_target_desired_distance = 40.0
 
 signal on_killed()
 
@@ -24,7 +26,7 @@ func _ready():
 
 func actor_setup():
 	await get_tree().physics_frame
-	print(str(object_to_go))
+	#print(str(object_to_go))
 	set_movement_target(object_to_go)
 	#print("masuk actor setup")
 
@@ -32,17 +34,30 @@ func set_movement_target(object):
 	navigation_agent.target_position = object.position
 
 func _physics_process(delta):
-	#print(navigation_agent.is_navigation_finished())
+	actor_setup()
 	if navigation_agent.is_navigation_finished():
 		return
-	var current_agent_position : Vector2 = global_position
-	var next_path_position : Vector2 = navigation_agent.get_next_path_position()
-
-	var new_velocity : Vector2 = next_path_position - current_agent_position
-	new_velocity = new_velocity.normalized()
-	new_velocity = new_velocity * base_speed
-
-	velocity = new_velocity
+		
+	#var current_agent_position : Vector2 = global_position
+	#var next_path_position : Vector2 = navigation_agent.get_next_path_position()
+	#var new_velocity : Vector2 = next_path_position - current_agent_position
+	#new_velocity = new_velocity.normalized()
+	#new_velocity = new_velocity * base_speed * delta
+	#velocity = new_velocity
+	
+	var direction = Vector2()
+	direction = navigation_agent.get_next_path_position() - global_position
+	direction = direction.normalized()
+	velocity = velocity.lerp(direction * static_speed, 1)
+	
+	var is_at_desired_distance :bool = (global_position.distance_to(navigation_agent.target_position) - navigation_agent.target_desired_distance) <= 0.0
+	print(is_at_desired_distance)
+	if is_at_desired_distance:
+		velocity = Vector2.ZERO
+	
+	#var direction = Vector2.AXIS_X
+	#velocity = (position + Vector2(1,0)) * 10 * delta
+	
 	move_and_slide()
 
 func getHit(damage:float, projectile_penetration:int):
