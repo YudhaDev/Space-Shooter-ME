be93560412b4f1c3d5d78749db5a5b3495f5d2da
YudhaDev <33086490+YudhaDev@users.noreply.github.com>
2024-06-22 04:35:38 +0700

    definisi buff

--- scripts/global/GlobalEnvironment.gd
+++ scripts/global/GlobalEnvironment.gd
@@ -1,3 +1,9 @@
 extends Node
 
 var isPlayerInBonfire : bool = false
+
+var playerBasedBuffs = ["player01:hp-up", "player02:speed-up", "player03:attack-mult-up"]
+var weaponBounceBasedBuffs = ["weapon-bounce01:bounce", "weapon-bounce-enhance01:base-attack-up", "weapon-bounce-enhance02:base-projectile-speed-up"]
+var weaponPenetrateBasedBuff = ["weapon-penetrate01:penetrate"]
+
+var buffs = [playerBasedBuffs, weaponBounceBasedBuffs, weaponPenetrateBasedBuff]
