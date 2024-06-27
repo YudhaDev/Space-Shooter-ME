f7d8c849996890e5d9b2fef99c3d67e6ccb67211
YudhaDev <33086490+YudhaDev@users.noreply.github.com>
2024-06-27 20:00:32 +0700

    progress restruktur scene

--- scenes/ui/script_buff_selection.gd
+++ scenes/ui/script_buff_selection.gd
@@ -33,7 +33,17 @@ func generateBuffOption():
 	pass
 
 func _on_button_4_pressed():
-	#var tampung = global_environment.maxedBuffExclusion()
-	#print(str(tampung))
-	print("oyyy: "+str(global_environment.pickBuff()))
+	var tampung = global_environment.pickBuff()
+	for item in tampung:
+		var split = item.split(":")
+		var build_string = "%s | %s" % [split[0], split[1]]
+		
+		var button := Button.new()
+		button.text = build_string
+		button.pressed.connect(dynamic_buttons_on_pressed.bind(item))
+		$VBoxContainer.add_child(button)
 	pass # Replace with function body.
+
+func dynamic_buttons_on_pressed(buff):
+	print("buff %s terpilih." % buff.split(":")[1])
+	pass
